5f61b5637216feb5a6976f4e794ccee0c0939af8
Aash048 <145459769+Aash048@users.noreply.github.com>
2023-09-19 18:49:06 +0530

    Add files via upload

---
+++ Fibonacci and list problem.py
@@ -0,0 +1,30 @@
+#fibanocci sequence :
+
+n = 13
+fib_sequence = [0, 1]
+
+while len(fib_sequence) < n:
+    fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+print(fib_sequence)
+
+
+
+#getting positive integers:
+
+list1= [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+new_list = [str(num) for num in list1 if num >= 0]
+string = ', '.join(new_list)
+print(string)
+
+
+
+modified_list = []
+
+for num in list2:
+    if num >= 0:
+        modified_list.append(num)
+
+print(modified_list)
